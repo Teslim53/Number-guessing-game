cbf13f4ca36eb19c46ac5ea3a90901f0d11163ef
Teslim Lawal <95858261+Teslim53@users.noreply.github.com>
2023-01-20 16:10:40 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+import random
+
+random_number = random.randint(1, 99)
+
+app = Flask(__name__)
+
+# Create required routes
+# Home route
+@app.route("/", methods=['GET'])
+def index():
+    return render_template("index.html")
+
+# Route to choose number
+@app.route("/game", methods=['GET'])
+def game_first():
+    return render_template("game.html", random_number=random_number)
+
+# Compare number chosen by user to random number and return appropriate page
+@app.route("/game/<int:number_chosen>", methods=['GET'])
+def game(number_chosen):
+    if number_chosen < random_number:
+        return render_template("low.html", number_chosen=number_chosen)
+    elif number_chosen > random_number:
+        return render_template("high.html", number_chosen=number_chosen)
+    elif number_chosen == random_number:
+        return render_template("correct_number.html", number_chosen=number_chosen, random_number=random_number)
